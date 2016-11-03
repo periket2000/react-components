3e295f67e1f90ef8b9f44f30e48f848b43fb5f3e
Marco Albero <marco@mybi.es>
2016-11-03 17:33:17 +0100

    react components

---
+++ c.js
@@ -0,0 +1,10 @@
+//helloworld.js
+import React from 'react';
+class ReactHelloWorld extends React.Component {
+  render() {
+    return (
+      <div> Hello world!! </div>
+    )
+  }
+}
+export default ReactHelloWorld;
