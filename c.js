4bf8c2455cd703f689a836ce8a278c774c094ce7
Marco Albero <marco@mybi.es>
2016-11-03 19:41:25 +0100

    composition test

--- c.js
+++ c.js
@@ -1,13 +1,7 @@
 import SimpleButton from './lib/buttons/simple'
-import React from 'react';
-class ReactHelloWorld extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello world!!
-        <SimpleButton />
-      </div>
-    )
-  }
-}
-export default ReactHelloWorld;
+
+var mybi = {
+    SimpleButton: SimpleButton,
+};
+
+module.exports = mybi;
